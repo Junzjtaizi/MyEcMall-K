1e7e50a9167b78634f3c8e5f3b68cf2fb3aa2dcd
NieKing Liu <jzjtaizi@qq.com>
2018-05-07 14:17:59 +0800

    First commit.

---
+++ MyEcMallK/App/src/main/java/cn/nieking/myecmallk/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package cn.nieking.myecmallk.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import cn.nieking.myecmallk.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
