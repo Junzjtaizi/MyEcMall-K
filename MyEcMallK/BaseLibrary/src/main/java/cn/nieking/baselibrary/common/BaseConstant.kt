59d5a25982b5497cc3137c0e00d68760384e37a5
NieKing <jzjtaizi@qq.com>
2018-05-08 23:50:53 +0800

    api dagger2.

--- MyEcMallK/BaseLibrary/src/main/java/cn/nieking/baselibrary/common/BaseConstant.kt
+++ MyEcMallK/BaseLibrary/src/main/java/cn/nieking/baselibrary/common/BaseConstant.kt
@@ -2,6 +2,6 @@ package cn.nieking.baselibrary.common
 
 class BaseConstant {
     companion object {
-        val SERVER_ADDRESS = "http://192.168.0.108:8080/"
+        val SERVER_ADDRESS = "http://192.168.123.49:8080/"
     }
 }
