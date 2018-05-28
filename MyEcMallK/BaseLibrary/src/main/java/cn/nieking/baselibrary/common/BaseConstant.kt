861feb1703e3b2ce03f74072675cb90c4563cf08
NieKing <jzjtaizi@qq.com>
2018-05-28 21:16:46 +0800

    Check.

--- MyEcMallK/BaseLibrary/src/main/java/cn/nieking/baselibrary/common/BaseConstant.kt
+++ MyEcMallK/BaseLibrary/src/main/java/cn/nieking/baselibrary/common/BaseConstant.kt
@@ -2,7 +2,7 @@ package cn.nieking.baselibrary.common
 
 class BaseConstant {
     companion object {
-        const val IMAGE_SERVER_ADDRESS = "http://osea2fxp7.bkt.clouddn.com/"
+        const val IMAGE_SERVER_ADDRESS = "http://120.79.59.193:8080/Kotlin_Server/"
         const val SERVER_ADDRESS = "http://192.168.123.49:8080/"
         const val TABLE_PREFS = "my_ec_mall_k"
         const val KEY_SP_TOKEN = "token"
