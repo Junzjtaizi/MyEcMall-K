28ceba43a0fe5c2ec00ee26f056acdaa10fd49d6
Junzjtaizi <jzjtaizi@qq.com>
2018-07-12 19:18:43 +0800

    Change Address.

--- MyEcMall-K/BaseLibrary/src/main/java/cn/nieking/baselibrary/common/BaseConstant.kt
+++ MyEcMall-K/BaseLibrary/src/main/java/cn/nieking/baselibrary/common/BaseConstant.kt
@@ -3,7 +3,7 @@ package cn.nieking.baselibrary.common
 class BaseConstant {
     companion object {
         const val IMAGE_SERVER_ADDRESS = "http://osea2fxp7.bkt.clouddn.com/"
-        const val SERVER_ADDRESS = "http://120.79.59.193:8080/Kotlin_Server/"
+        const val SERVER_ADDRESS = "http://139.159.161.40:8080/Kotlin_Server/"
         const val TABLE_PREFS = "my_ec_mall_k"
         const val KEY_SP_TOKEN = "token"
     }
